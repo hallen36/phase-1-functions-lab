817b681b533fa9669c3873da3742009986372dc3
John Hunter Allen <hallen36@gmail.com>
2021-12-11 13:31:09 -0500

    commit tree 4cc6999093e7ad96e300041476ff5f2a604c1e7e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number){
+    return Math.abs(number-42)
+}
+function distanceFromHqInFeet(number) {
+    return distanceFromHqInBlocks(number) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start-destination) * 264
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far'
+    } else if (distanceTravelledInFeet(start, destination) > 2000) {
+        return 25
+    } else if (distanceTravelledInFeet(start, destination) > 400) {
+        return (distanceTravelledInFeet(start, destination)-400) * .02
+    } else if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    }
+}
